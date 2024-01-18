a5ccb92eba2bc146cf21f505e456f38028f4fd7f
Edo <hello@edoardo-hb.it>
2024-01-18 17:10:17 +0100

    feat: Completed Jumbotron

--- src/components/main/Jumbotron.vue
+++ src/components/main/Jumbotron.vue
@@ -1,24 +1,49 @@
 <script>
 export default {
-  name: 'Jumbotron',
-}
+  name: "Jumbotron",
+};
 </script>
 
 <template>
   <div class="hero">
     <div class="hero__content">
       <p class="intro-text text-red">Certified fitness professional</p>
-      <h1 class="big-text">Take control of your health</h1>
-      <p class="intro-text-2">Curabitur non nulla sit amet nisl tempus convallis quis ac lectus dolor sit amet.</p>
+      <h1 class="big-text pb-5">Take control of your health</h1>
+      <div>
+        <img src="/leila-waves-yellow.png" alt="" />
+        <img
+          src="https://cdn-icons-png.flaticon.com/512/47/47371.png"
+          class="d-none"
+          alt=""
+        />
+      </div>
+      <p class="intro-text-2 pt-5">
+        Curabitur non nulla sit amet nisl tempus convallis quis ac lectus dolor
+        sit amet.
+      </p>
 
-      <button class="button_ bg-blue px-4 py-2 text-center">Visit my YouTube channel</button>
-      <button class="button_ bg-outline px-4 py-2 text-center">Buy Avada Today &rarr;</button>
+      <button class="button_ py-3 mx-4 bg-blue px-4 py-2 text-center">
+        <i class="fa-brands fa-youtube mx-2"></i>
+        Visit my YouTube channel
+      </button>
+      <button class="button_ py-3 piccolo-bug bg-outline px-4 py-2 text-center">
+        Buy Avada Today
+        <i class="fa-solid mx-2 fa-arrow-right"></i>
+      </button>
     </div>
   </div>
+
+  <link
+    rel="stylesheet"
+    href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css"
+    integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA=="
+    crossorigin="anonymous"
+    referrerpolicy="no-referrer"
+  />
 </template>
 
 <style lang="scss" scoped>
-@use '../../styles/partials/mixins' as *;
+@use "../../styles/partials/mixins" as *;
 @use "../../styles/partials/variables" as *;
 
 .hero {
@@ -26,7 +51,9 @@ export default {
   display: flex;
   width: 100%;
   align-items: center;
-  background: linear-gradient(90deg, black 47%,rgba(6, 6, 7, 0) 76%), url('https://avada.website/fitness/wp-content/uploads/sites/130/2020/02/slider82x-scaled.jpg') no-repeat;
+  background: linear-gradient(90deg, black 47%, rgba(6, 6, 7, 0) 76%),
+    url("https://avada.website/fitness/wp-content/uploads/sites/130/2020/02/slider82x-scaled.jpg")
+      no-repeat;
   background-size: cover;
 }
 
@@ -58,7 +85,7 @@ export default {
 
 .bg-outline {
   background-color: transparent !important;
-  border: 2px solid $grey !important;
+  border: 3.23px solid rgb(37, 37, 37) !important;
 }
 
 .button_ {
