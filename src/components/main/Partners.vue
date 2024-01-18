08e96ec51a4bb88561d77839fd0f4c2fa98bc3cb
n3ndo <ferdinandof3lixbagnale@gmail.com>
2024-01-18 11:15:16 +0100

    fix: email button

--- src/components/main/Partners.vue
+++ src/components/main/Partners.vue
@@ -6,14 +6,14 @@ export default {
 <template lang="">
   <div class="bg-img">
     <div class="container">
-      <div class="row py-5">
-        <h4>Join our mailing list today</h4>
-        <h1 class="text-white">Insider offers & flash sales in your inbox every week</h1>
+      <div class="row width-80 py-5">
+        <p class="red size-28">Join our mailing list today</p>
+        <p class="text-white size-58">Insider offers & flash sales in your inbox every week</p>
         <div class="row pt-5">
-          <div class="col-6">
-            <input type="email" name="email" id="email" value="" required="true"  placeholder="Insert your email ...*" class="email">
+          <div class="col-8">
+            <input type="email" name="email" id="email" value="" required="true"  placeholder="Insert your email ...*" class="form-control email">
           </div>
-          <div class="col-3">
+          <div class="col-4">
             <button type="submit" class="button">Subscribe</button>
           </div>
         </div>
@@ -29,29 +29,47 @@ export default {
 
 .bg-img {
   background-size: cover;
-  background-image: linear-gradient(90deg, black 47%,rgba(6, 6, 7, 0) 76%), url('../../../public/banner1-2x.jpg');
+  background-image: linear-gradient(90deg, black 47%, rgba(6, 6, 7, 0) 76%), url('../../../public/banner1-2x.jpg');
 
-  .row{
+  .container {
     width: 80%;
+    margin-left: 200px;
+    padding: 95px 111px;
+
+    .size-58 {
+      font-size: 58px;
+    }
+
+    .size-28 {
+      font-size: 28px;
+    }
+
+    .red {
+      color: $red;
+    }
+
+    .width-80 {
+      width: 80%;
+    }
+
+    .email {
+      padding: 15px 60px;
+      border-radius: 40px;
+      border: 1px solid black;
+      font-size: 18px;
+    }
+
+    .button {
+      @include blue-button;
+      padding: 17px 60px;
+    }
+
+    .paragraph {
+      color: $grey;
+      font-size: 23px;
+    }
   }
-  h4 {
-    color: $red;
-  }
-  
-  .email {
-    padding: 15px 60px;
-    border-radius: 40px;
-    border: 1px solid black;
-    font-size: 18px;
-  }
-  
-  .button {
-    @include blue-button;
-  }
-  
-  .paragraph {
-    color: $grey;
-    font-size: 23px;
-  }
+
+
 }
 </style>
