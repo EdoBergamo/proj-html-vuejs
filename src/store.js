9c5179294544f027e6d423890ab861463af69acc
SebastianBuftea <sebibuftea85@gmail.com>
2024-01-18 11:02:20 +0100

    [UPDATE] add dropdown

--- src/store.js
+++ src/store.js
@@ -6,37 +6,48 @@ export const store = reactive({
       id: 1,
       label: "Home",
       href: '/',
-      active: true
+      active: true,
+      dropdown: [],
     },
     {
       id: 2,
       label: "Services",
       href: '/',
-      active: false
+      active: false,
+      dropdown: ['All services', 'Service Single Page'],
+
     },
     {
       id: 3,
       label: "About",
       href: '/',
-      active: false
+      active: false,
+      dropdown: ['About me', 'Our sponsor', 'Contact'],
+
     },
     {
       id: 1,
       label: "Videos",
       href: '/',
-      active: false
+      active: false,
+      dropdown: ['All Playlist', 'Playlist Page', 'Video Single Page'],
+
     },
     {
       id: 1,
       label: "Blog",
       href: '/',
-      active: false
+      active: false,
+      dropdown: false,
+
     },
     {
       id: 1,
       label: "Store",
       href: '/',
-      active: false
+      active: false,
+      dropdown: ['Simple Product', 'Variable Product'],
+
     },
   ],
 
