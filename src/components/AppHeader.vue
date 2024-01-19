8f010fa9eeef702f51be9b093d5c164c481438ec
SebastianBuftea <sebibuftea85@gmail.com>
2024-01-19 21:13:56 +0100

    [UPDATE] add rotate effect in search bar

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -37,9 +37,9 @@ export default {
 </script>
 
 <template>
-  <header class="d-flex align-items-center">
-    <div class="container">
-      <div class="row d-flex justify-content-between align-items-center">
+  <header class="d_flex">
+    <div class="container_header">
+      <div class="row_ ">
 
         <!-- logo link -->
         <div class="w_auto">
@@ -48,33 +48,34 @@ export default {
               alt="logo avada fitness">
           </a>
         </div>
+        <div class="container_ ">
+          <div class="box_" :class="SearchActive ? 'change_' : ''">
 
-        <div class="w_auto d_flex align-items-center transition_" v-show="SearchActive == false" id="section1">
-          <!-- links -->
-          <ul class="d-flex m-0">
-            <AppLink v-for="(link, index) in  store.header " :key="index" class="m-2" @click="ActiveLink(link.id)"
-              :link="link"></AppLink>
-          </ul>
+            <div class="upside_ btn_">
+              <div class="input-group w_auto m-0 w-100">
+                <input type="text" class="p-3 search_style" placeholder="Search..." aria-label="Username"
+                  aria-describedby="basic-addon1">
+                <span class="input-group-text button_search " id="basic-addon1">
+                  <i class="fa-solid fa-magnifying-glass m-1 fa-lg"></i>
+                </span>
+              </div>
+              <i class="fa-solid fa-xmark icon_grey mx-4 fa-2xl" @click="SearchOn"></i>
+            </div>
 
-          <!-- button and searchbutton -->
-          <span>
-            <button class="button_ px-4 py-2 text-center mx-3">Schedule a workout</button>
-          </span>
-          <i class="fa-solid fa-magnifying-glass mx-3 icon_grey" @click="SearchOn"></i>
+            <div class="front_ btn_">
+              <ul class="d_flex m-0">
+                <AppLink v-for="(link, index) in  store.header " :key="index" class="m-2" @click="ActiveLink(link.id)"
+                  :link="link"></AppLink>
+              </ul>
 
-          <i class="fa-solid fa-cart-shopping mx-3 icon_grey"></i>
-        </div>
-
-        <!-- search bar activate -->
-        <div v-show="SearchActive" class="d_flex  align-items-center w-75" id="section2">
-          <div class="input-group w_auto m-0 w-100">
-            <input type="text" class="p-3 search_style" placeholder="Search..." aria-label="Username"
-              aria-describedby="basic-addon1">
-            <span class="input-group-text button_search " id="basic-addon1">
-              <i class="fa-solid fa-magnifying-glass m-1 fa-lg"></i>
-            </span>
+              <!-- button and searchbutton -->
+              <span>
+                <button class="button_ px-4 py-2 text-center mx-3">Schedule a workout</button>
+              </span>
+              <i class="fa-solid fa-magnifying-glass mx-3 icon_grey" @click="SearchOn"></i>
+              <i class="fa-solid fa-cart-shopping mx-3 icon_grey"></i>
+            </div>
           </div>
-          <i class="fa-solid fa-xmark icon_grey mx-4 fa-2xl" @click="SearchOn"></i>
         </div>
       </div>
     </div>
@@ -85,63 +86,104 @@ export default {
 @use '../styles/partials/variables' as*;
 @use '../styles/partials/mixins' as*;
 
-html {
-  scroll-behavior: smooth;
-}
-
 header {
   background-color: $black;
   height: 100px;
 
-  ul {
-    list-style-type: none;
+  .container_header {
+    width: 95vw;
+    margin: 0 auto;
 
-    i {
-      color: $grey;
+    .row_ {
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+
+
+      ul {
+        list-style-type: none;
+        height: 60px;
+        display: flex;
+        align-items: center;
+
+        i {
+          color: $grey;
+        }
+      }
+
+      .button_ {
+        @include blue-button;
+        font-weight: normal;
+        font-size: medium;
+        background-color: $blue;
+        transition-property: background-color;
+        transition-duration: 1s;
+
+        &:hover {
+          background-color: $red;
+        }
+      }
+
+      .container_ {
+        display: flex;
+        align-items: center;
+        height: 95px;
+
+        .change_ {
+          transform: rotateX(90deg)
+        }
+
+        .box_ {
+          transform-style: preserve-3d;
+          transition: 0.4s ease;
+
+          .btn_ {
+            background-color: rgb(0, 0, 0);
+            color: white;
+            display: flex;
+            align-items: center;
+            backface-visibility: hidden;
+          }
+
+          .upside_ {
+            transform: rotateX(-90deg) translateZ(75px);
+
+            .search_style {
+              width: 90%;
+              height: 60px;
+              border-radius: 50px 0px 0px 50px;
+            }
+
+            .button_search {
+              border-radius: 0px 50px 50px 0px;
+              background-color: $black;
+              color: white;
+              border: none;
+
+              &:hover {
+                background-color: $blue;
+              }
+            }
+
+            .icon_grey {
+              color: $grey;
+              transition-property: color;
+              transition-duration: 1s;
+
+              &:hover {
+                color: rgb(255, 255, 255);
+              }
+            }
+          }
+
+          .front_ {
+            transform: translateZ(75px) translateY(-30px);
+
+          }
+        }
+      }
     }
   }
-
-  .button_ {
-    @include blue-button;
-    font-weight: normal;
-    font-size: medium;
-    background-color: $blue;
-    transition-property: background-color;
-    transition-duration: 1s;
-
-    &:hover {
-      background-color: $red;
-    }
-  }
-
-  .icon_grey {
-    color: $grey;
-    transition-property: color;
-    transition-duration: 1s;
-
-    &:hover {
-      color: rgb(255, 255, 255);
-    }
-  }
-
-  .search_style {
-    width: calc(100% - 60px);
-    height: 60px;
-    border-radius: 50px 0px 0px 50px;
-  }
-
-  .button_search {
-    border-radius: 50px;
-    background-color: $black;
-    color: white;
-    border: none;
-
-    &:hover {
-      background-color: $blue;
-    }
-  }
-
-
 }
 </style>
 
