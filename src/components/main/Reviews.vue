10f7748c898f5c7118978ae53e6565bfe4e89f5a
stappost <emanuele.scarpantoni@gmail.com>
2024-01-19 13:56:17 +0100

    end reviews component

--- src/components/main/Reviews.vue
+++ src/components/main/Reviews.vue
@@ -5,19 +5,35 @@ export default {
 </script>
 
 <template>
-  <div class="bg_linear">
+  <div class="bg_linear ">
     <div class="container leila">
-      <div class="row width-80 py-5">
-        <div class="image-section">
-          <img src="https://avada.website/fitness/wp-content/uploads/sites/130/2019/11/service6@2x.jpg" alt="Image 1" />
-
-          <div class="bottom-left mx-4">
-            <img src="/big-wave-grossa-gls.png" />
-            <h1>Crossfit Workout</h1>
-            <p class="text-yellow">Push your limits</p>
+      <div class="row justify-content-center">
+        <div class="col-4">
+          <div class="my_card" id="my_card-1">
+            <div class="layout">
+              <img src="/big-wave-grossa-gls.png" alt="">
+              <h2>Crossfit workout</h2>
+              <span>Push your limits</span>
+            </div>
           </div>
-          <img src="https://avada.website/fitness/wp-content/uploads/sites/130/2019/11/box1@2x.jpg" alt="Image 2" />
-          <img src="https://avada.website/fitness/wp-content/uploads/sites/130/2019/11/box3@2x.jpg" alt="Image 3" />
+        </div>
+        <div class="col-4">
+          <div class="my_card" id="my_card-2">
+            <div class="layout">
+              <img src="/big-wave-grossa-gls.png" alt="">
+              <h2>New gym apparel</h2>
+              <span>Look good, feel good</span>
+            </div>
+          </div>
+        </div>
+        <div class="col-4">
+          <div class="my_card" id="my_card-3">
+            <div class="layout">
+              <img src="/big-wave-grossa-gls.png" alt="">
+              <h2>Team training</h2>
+              <span>Find a partner</span>
+            </div>
+          </div>
         </div>
 
         <div class="text-section">
@@ -30,14 +46,16 @@ export default {
         <img src="/leila.jpg" class="d-none" alt="" />
 
         <div class="d-flex align-items-centerx justify-content-center">
-          <img src="/leila-waves-yellow.png" class="" alt="" />
+          <img src="/leila-waves-yellow.png" class="my-3" />
         </div>
 
-        <div class="d-flex justify-content-center mt-4">
+        <div class="d-flex justify-content-center mt-4 text-light align-item-center">
           <img class="image" src="https://avada.website/fitness/wp-content/uploads/sites/130/2020/03/avatar-1@2x.jpg"
             alt="" />
-          <p>Brad Johnson</p>
-          <p>Las Vegas</p>
+          <div class="name d-flex gap-3">
+            <p class="text-light">Brad Johnson</p>
+            <p class="text-secondary">Las Vegas</p>
+          </div>
         </div>
       </div>
     </div>
@@ -49,51 +67,85 @@ export default {
 
 .bg_linear {
   background: linear-gradient(150deg, rgb(0, 22, 219) 0%, rgb(219, 0, 11) 100%);
-  ;
+  padding-bottom: 200px;
 
-  .leila {
+  .my_card {
     position: relative;
-    text-align: center;
-    color: #fff;
-
-    .bottom-left {
-      position: absolute;
-      left: 16px;
-      top: 220px;
-    }
-  }
-
-  .text-yellow {
-    color: $yellow;
-  }
-
-  .image-section {
-    display: flex;
-    justify-content: space-around;
+    min-height: 500px;
+    border-radius: 10px;
+    border-bottom-right-radius: 40px;
     margin-top: -100px;
 
-    img {
-      width: 30%;
-      border-radius: 8px;
+    .layout {
+      position: absolute;
+      width: 100%;
+      height: 100%;
+      top: 0;
+      left: 0;
+      background: linear-gradient(180deg, rgba(0, 0, 0, 0.229) 10%, black 100%);
+      padding-left: 20px;
+
+
+      img {
+        margin-top: 300px;
+        filter: saturate(100%);
+        width: 20%;
+      }
+
+      h2 {
+        margin-top: 20px;
+        color: white;
+        font-size: 40px;
+      }
+
+      span {
+        color: $yellow;
+        font-size: 25px;
+      }
     }
+  }
+
+  #my_card-1 {
+    background: url('https://avada.website/fitness/wp-content/uploads/sites/130/2019/11/service6@2x.jpg');
+    background-size: cover;
+    height: fit-content;
+  }
+
+  #my_card-2 {
+    background: url('https://avada.website/fitness/wp-content/uploads/sites/130/2019/11/box1@2x.jpg');
+    background-size: cover;
+    height: fit-content;
+  }
+
+  #my_card-3 {
+    background: url('https://avada.website/fitness/wp-content/uploads/sites/130/2019/11/box3@2x.jpg');
+    background-size: cover;
+    height: fit-content;
+
   }
 
   .text-section {
     text-align: center;
     margin: 20px;
+    margin-top: 60px;
 
     p {
-      font-size: 16px;
+      color: white;
+      font-size: 50px;
     }
 
-    .big-text {
-      font-size: 40px;
-      color: #000;
-    }
+    .big-text {}
   }
 
   .image {
     border-radius: 50%;
+    width: 70px;
+    height: 70px;
+  }
+
+  .name {
+    font-size: 30px;
+    padding: 10px 20px;
   }
 }
 </style>
